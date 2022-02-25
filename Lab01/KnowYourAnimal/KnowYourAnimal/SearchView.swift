000a5c5e4210af8f150a01e79e830c7cb6fb24a9
tobiasritscher <tobias@ritscher.ch>
2022-02-25 19:34:28 +0100

    added missing files

--- Lab01/KnowYourAnimal/KnowYourAnimal/SearchView.swift
+++ Lab01/KnowYourAnimal/KnowYourAnimal/SearchView.swift
@@ -8,13 +8,63 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State private var interpret: String = ""
+    @State private var text = ""
+    
+    @Binding var results: [Result]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Text("Interpret: ")
+            TextField("type here...", text: $interpret)
+        }.padding()
+        
+        Button("Search"){
+            print("Button pressed")
+            text = "loading...."
+            Task.init {
+                if await loadData(interpret: interpret) {
+                    text = "success"
+                } else {
+                    text = "failed: wrong URL or invalid Data"
+                }
+            }
+        }
+        
+        Text(text)
+    }
+    
+    
+    func loadData(interpret: String) async -> Bool {
+        let interpretCorrected = interpret.replacingOccurrences(of: " ", with: "+")
+        guard let url = URL(string: "https://itunes.apple.com/search?term=\(interpretCorrected)&entity=song") else {
+            print("Invalid URL")
+            return false
+        }
+        do {
+            print("loading data")
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.results
+            }
+            return true
+        } catch {
+            print("Invalid data")
+            return false
+        }
     }
 }
 
-struct SearchView_Previews: PreviewProvider {
-    static var previews: some View {
-        SearchView()
-    }
+
+
+struct Response: Codable {
+    var results: [Result]
 }
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
