ab283697137bdb1492b346790d37a89c17658945
zossluk1 <zossluk1@students.zhaw.ch>
2022-03-15 11:32:07 +0100

    add display of functioning list

--- Lab04/KnowYourAnimalPrototype/App.tsx
+++ Lab04/KnowYourAnimalPrototype/App.tsx
@@ -1,11 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+
+const AnimalData = [
+  {
+    id: 1,
+    title: 'Tiger',
+  },
+  {
+    id: 2,
+    title: 'Panther',
+  },
+  {
+    id: 3,
+    title: 'Rat',
+  },
+  {
+    id: 4,
+    title: 'Elefant',
+  },
+  {
+    id: 5,
+    title: 'Flamingo',
+  },
+  {
+    id: 6,
+    title: 'Maus',
+  },
+  {
+    id: 7,
+    title: 'Luchs',
+  },
+  {
+    id: 8,
+    title: 'Nashorn',
+  },
+  {
+    id: 9,
+    title: 'Marder',
+  },
+  {
+    id: 10,
+    title: 'Puma',
+  }
+];
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.sectionHeader}>Know your Animal Prototype!</Text>
       <StatusBar style="auto" />
+      <FlatListAnimals/>
     </View>
   );
 }
@@ -13,8 +58,53 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+    paddingTop: 22
+  },
+  sectionHeader: {
+    paddingTop: 50,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
+  },
+  item: {
+    padding: 10,
+    fontSize: 20,
+    height: 60,
   },
 });
+
+const FlatListAnimals = () => {
+  return (
+      <View style={styles.container}>
+        <FlatList
+            data={AnimalData}
+            renderItem={({ item }) => <ItemRender title={item.title} />}
+            ItemSeparatorComponent={ItemDivider}
+        />
+      </View>
+  );
+}
+
+// @ts-ignore
+const ItemRender = ({ title }) => (
+    <View>
+      <Text style={styles.item}>{title}</Text>
+    </View>
+);
+
+const ItemDivider = () => {
+  return (
+      <View
+          style={{
+            height: 1,
+            width: "100%",
+            backgroundColor: "#607D8B",
+          }}
+      />
+  );
+}
+
