902b88169eeb41302dd45b3c28c9baec0091d70d
joeck <joelplambeck@gmail.com>
2022-05-03 11:42:46 +0200

    lab11

---
+++ Lab11/KnowYourMusic/KnowYourMusic/KnowYourMusicApp.swift
@@ -0,0 +1,20 @@
+
+//
+//  Created by Tobias Ritscher on 22.02.22.
+//  Approved by Joël Plambeck on 03.05.22. :-)
+//
+
+import SwiftUI
+
+@main
+struct KnowYourMusicApp: App {
+    let persistenceController = PersistenceController.shared
+    @State var results = [Result]()
+
+    var body: some Scene {
+        WindowGroup {
+            SearchView(results: $results)
+
+        }
+    }
+}
